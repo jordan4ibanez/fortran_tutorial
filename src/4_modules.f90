9f1c58961422f76e6641d16c9bbaefb79e3332b8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-02 07:52:38 -0400

    Let us make this more interactive

---
+++ src/4_modules.f90
@@ -0,0 +1,11 @@
+module modules
+  ! This is a module. A module is one of the lowest building blocks of Fortran.
+  !
+  ! A module is very simple. You can put your code in it.
+  !
+  !
+  !
+  
+
+
+end module modules
