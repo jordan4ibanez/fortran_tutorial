696fefca8de9c3b518eced61c89ec27640fd5a8f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-02 08:11:30 -0400

    Update 5_variables_and_parameters.f90

--- src/5_variables_and_parameters.f90
+++ src/5_variables_and_parameters.f90
@@ -5,6 +5,34 @@ module variables_and_parameters
   private ! I always space things out by 2 lines. I just like the way it looks. :)
 
 
+  ! A private module variable.
+  !
+  ! This can be changed throughout the module.
+  !
+  ! It is only visible in the module.
+  integer :: my_secret_variable
+
+  ! A public module variable.
+  !
+  ! This can be changed throughout the module and in any module/program that imports it.
+  !
+  ! It is visible to anything that imports it.
+  integer, public :: a_public_variable
+
+
+  ! A parameter follows the same visibility rules as a variable with one slight
+  ! difference: It cannot be changed.
+  !
+  ! In other languages, this is known as a constant.
+  !
+  ! It must be defined as well.
+  integer, parameter :: immutable_secret_magic_number = 5
+
+
+contains
+
+
+
 
   !!(different basic types)
 
